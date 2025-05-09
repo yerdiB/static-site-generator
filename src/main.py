bfa1c975403f08df20c2110925721ea610bedcbd
yerdiB <yerdi.isman.com>
2025-05-09 20:22:44 +0500

    add build.sh for production

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import shutil, os
+import shutil, os, sys
 from block_markdown import *
 
 def src_to_dest(static, public):
@@ -16,7 +16,7 @@ def extract_title(markdown):
             return line[2:]
     raise Exception("no heading")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     if dest_path.endswith(".md"):
         dest_path = f"{dest_path[:-2]}html"
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
@@ -33,28 +33,33 @@ def generate_page(from_path, template_path, dest_path):
     
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", content)
+    template = template.replace("href=\"/", f"href=\"{basepath}")
+    template = template.replace("src=\"/", f"src=\"{basepath}")
     
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     
     with open(dest_path, "w") as f:
         f.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for each in os.listdir(dir_path_content):
         if os.path.isfile(os.path.join(dir_path_content, each)):
             if each.endswith(".md"):
-                generate_page(os.path.join(dir_path_content, each), template_path, os.path.join(dest_dir_path, each))
+                generate_page(os.path.join(dir_path_content, each), template_path, os.path.join(dest_dir_path, each), basepath)
         else:
             os.makedirs(os.path.join(dest_dir_path, each), exist_ok=True)
-            generate_pages_recursive(os.path.join(dir_path_content, each), template_path, os.path.join(dest_dir_path, each))
+            generate_pages_recursive(os.path.join(dir_path_content, each), template_path, os.path.join(dest_dir_path, each), basepath)
 
 
 def main():
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-        os.mkdir("public")
-    src_to_dest("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    if os.path.exists("docs"):
+        shutil.rmtree("docs")
+    os.mkdir("docs")
+    src_to_dest("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
     return
 
 main()
